68732c5ecebe932a4e9820e4c7f884b29410ad5e
BharathKumar410 <bharatah14741@gmail.com>
2021-05-04 11:18:00 +0530

    added new

---
+++ Test_Three.java
@@ -0,0 +1,14 @@
+package com.vbk.app.scripts;
+
+import org.testng.annotations.Test;
+
+public class Test_Three {
+
+	@Test
+	public void test() {
+		System.out.println("hello");
+		System.out.println("plzz do practice");
+	}
+	
+	
+}
